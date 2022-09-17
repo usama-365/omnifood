8be0a5b8b127f2cb001e91b181dd682a26ffddfa
usama-365 <usama-365@protonmail.com>
2022-09-17 21:12:11 +0500

    Made smooth scrolling work

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 document.querySelector('.current-year').innerText = new Date().getFullYear();
 // Make the mobile navigation work
 const navBtn = document.querySelector('.btn-mobile-nav');
-navBtn.sty
 const header = document.querySelector('.header');
 navBtn.addEventListener('click', () => {
     header.classList.toggle('nav-open');
+});
+
+header.querySelectorAll('.main-nav-link').forEach((a) => {
+    a.addEventListener('click', () => {
+        header.classList.remove('nav-open');
+    });
 });
